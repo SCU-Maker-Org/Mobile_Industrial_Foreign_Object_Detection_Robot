4e5d2da65dac9f4b9681c29a667e0c7cb3d73432
Qianhao Sun <154659308+StrayerSQH@users.noreply.github.com>
2026-09-24 20:13:12 +0800

    更新：弃用F103版本代码，保留以防万一

---
+++ Vehicle_Code/Tracked_407/Core/Src/gpio.c
@@ -0,0 +1,92 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file    gpio.c
+  * @brief   This file provides code for the configuration
+  *          of all used GPIO pins.
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2026 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Includes ------------------------------------------------------------------*/
+#include "gpio.h"
+
+/* USER CODE BEGIN 0 */
+
+/* USER CODE END 0 */
+
+/*----------------------------------------------------------------------------*/
+/* Configure GPIO                                                             */
+/*----------------------------------------------------------------------------*/
+/* USER CODE BEGIN 1 */
+
+/* USER CODE END 1 */
+
+/** Configure pins as
+        * Analog
+        * Input
+        * Output
+        * EVENT_OUT
+        * EXTI
+*/
+void MX_GPIO_Init(void)
+{
+
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+
+  /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOE_CLK_ENABLE();
+  __HAL_RCC_GPIOC_CLK_ENABLE();
+  __HAL_RCC_GPIOH_CLK_ENABLE();
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+  __HAL_RCC_GPIOB_CLK_ENABLE();
+  __HAL_RCC_GPIOD_CLK_ENABLE();
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOE, Motor_B_IN1_Pin|Motor_B_IN2_Pin|BLUE_LED_Pin|Motor_C_IN2_Pin
+                          |Motor_C_IN1_Pin|Motor_D_IN2_Pin|Motor_D_IN1_Pin, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(HEAD_LED_GPIO_Port, HEAD_LED_Pin, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOB, Motor_A_IN2_Pin|Motor_A_IN1_Pin, GPIO_PIN_RESET);
+
+  /*Configure GPIO pins : Motor_B_IN1_Pin Motor_B_IN2_Pin BLUE_LED_Pin Motor_C_IN2_Pin
+                           Motor_C_IN1_Pin Motor_D_IN2_Pin Motor_D_IN1_Pin */
+  GPIO_InitStruct.Pin = Motor_B_IN1_Pin|Motor_B_IN2_Pin|BLUE_LED_Pin|Motor_C_IN2_Pin
+                          |Motor_C_IN1_Pin|Motor_D_IN2_Pin|Motor_D_IN1_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOE, &GPIO_InitStruct);
+
+  /*Configure GPIO pin : HEAD_LED_Pin */
+  GPIO_InitStruct.Pin = HEAD_LED_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(HEAD_LED_GPIO_Port, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : Motor_A_IN2_Pin Motor_A_IN1_Pin */
+  GPIO_InitStruct.Pin = Motor_A_IN2_Pin|Motor_A_IN1_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+
+}
+
+/* USER CODE BEGIN 2 */
+
+/* USER CODE END 2 */
